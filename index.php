efd162736de7211578d639849557f64c0e5d5dbd
Sudhansu Shrestha <32022715+shresthas22@users.noreply.github.com>
2018-04-20 19:35:33 -0400

    homepage prototype

---
+++ index.php
@@ -0,0 +1,33 @@
+<html lang="en">
+
+      <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="app.css">
+        <link href="https://fonts.googleapis.com/css?family=Julius+Sans+One%7CRaleway:300,400" rel="stylesheet">
+        <link href="https://opensource.keycdn.com/fontawesome/4.7.0/font-awesome.min.css" rel="stylesheet">
+        <title>League Stats</title>
+      </head>
+
+      <body>
+        <div class="container">
+          <header class="header">
+            <div class="logo">
+              <img class="logo-image" src="images/logo_final.jpg" alt="Logo image">
+            </div>
+          </header>
+            <div class="body">
+                  <form action="userprofile.php" method="post">
+                      Enter your Username: <input name="name" type="text">
+                      <input type="submit">
+                  </form>
+            </div>
+            <footer class="footer">
+              <p>Copyright © 2018 Sudhansu Shrestha and Tony Wang</p>
+            </footer>
+        </div>
+
+
+      </body>
+
+</html>
---
+++ userprofile.php
@@ -0,0 +1,14 @@
+<html>
+
+      <head>
+            <title>League Stats</title>
+      </head>
+
+      <body>
+            <?php
+                $username = $_POST["name"];
+                echo "Hello " . $username;
+            ?>
+      </body>
+
+</html>

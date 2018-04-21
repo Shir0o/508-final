fc31c3120961f45589c0bb1263e3946507c9da0e
Sudhansu Shrestha <32022715+shresthas22@users.noreply.github.com>
2018-04-20 21:37:29 -0400

    CSS updates

--- userprofile.php
+++ userprofile.php
@@ -1,14 +1,35 @@
-<html>
+<html lang="en">
 
       <head>
-            <title>League Stats</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="app.css">
+        <link href="https://fonts.googleapis.com/css?family=Julius+Sans+One%7CRaleway:300,400" rel="stylesheet">
+        <link href="https://opensource.keycdn.com/fontawesome/4.7.0/font-awesome.min.css" rel="stylesheet">
+        <title>League Stats</title>
       </head>
 
       <body>
+          <div class="container2">
+            <header>
+              <h1> User-Profile </h1>
+            </header>
+
+            <div class="banner">
+              <h2> This banner will show user's most played champs </h2>
+            </div>
+          </div>
+          <div class="content">
+            <p>
             <?php
                 $username = $_POST["name"];
                 echo "Hello " . $username;
             ?>
+          </p>
+          </div>
+            <footer class="footer">
+              <p>Copyright © 2018 Sudhansu Shrestha and Tony Wang</p>
+            </footer>
       </body>
 
 </html>
